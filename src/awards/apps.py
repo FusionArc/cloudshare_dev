b516e1ad4aeed95eb5b549231666936296803fa6
David Barrow <dave@fusion-arc.uk>
2020-03-24 12:44:41 +0000

    updated to awards app

--- src/awards/apps.py
+++ src/awards/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class AwardConfig(AppConfig):
-    name = 'award'
+class AwardsConfig(AppConfig):
+    name = 'awards'
--- src/cloudshare_dev/settings.py
+++ src/cloudshare_dev/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.staticfiles',
 
-    'award'
+    'awards'
 )
 
 MIDDLEWARE = [
