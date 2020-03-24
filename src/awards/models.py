0c9a6e6b3f1cf93d5d1435c8ada87e514bbb65eb
David Barrow <dave@fusion-arc.uk>
2020-03-24 17:18:16 +0000

    Add media folder

--- src/awards/models.py
+++ src/awards/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Award(models.Model):
     title = models.CharField(max_length=60)
-    img = models.ImageField(upload_to='static/images')
+    img = models.ImageField(upload_to='images')
 
     def __str__(self):
         return self.title
